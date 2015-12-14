33d8205bbaf850bf2d532938f1a95965e240d7c0
go-ravi <tmgravi@gmail.com>
2015-12-14 16:29:07 +0545

    Initial commit

---
+++ app/src/main/java/com/techinflux/paryatak/EventAdapter.java
@@ -0,0 +1,89 @@
+package com.techinflux.paryatak;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by ravi on 12/14/15.
+ */
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+    List<MainActivity.Event> events;
+    Context context;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public EventAdapter(List<MainActivity.Event> events) {
+        this.events = events;
+    }
+    // Create new views (invoked by the layout manager)
+
+    @Override
+    public EventViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.card_main, parent, false);
+        // create ViewHolder
+        EventViewHolder evh = new EventViewHolder(v);
+        return evh;
+    }
+    // Replace the contents of a view (invoked by the layout manager)
+
+    @Override
+    public void onBindViewHolder(EventViewHolder holder, int position) {
+        // - get data from your itemsData at this position
+        // - replace the contents of the view with that itemsData
+        holder.eventName.setText(events.get(position).name);
+        holder.eventDate.setText(events.get(position).date);
+        holder.eventPlace.setText(events.get(position).place);
+        holder.eventInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context = v.getContext();
+                Intent intent = new Intent(context, EventInfoActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        holder.eventShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:share
+            }
+        });
+
+    }
+    // Return the size of your dataset (invoked by the layout manager)
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+    // inner class to hold a reference to each item of RecyclerView
+
+    public class EventViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView eventName, eventDate, eventPlace, eventShare, eventInfo;
+        ImageView eventPhoto;
+
+
+        public EventViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView) itemView.findViewById(R.id.cv);
+            eventName = (TextView) itemView.findViewById(R.id.event_name);
+            eventDate = (TextView) itemView.findViewById(R.id.event_date);
+            eventPlace = (TextView) itemView.findViewById(R.id.event_place);
+            eventShare = (TextView) itemView.findViewById(R.id.event_share);
+            eventInfo = (TextView) itemView.findViewById(R.id.event_info);
+            eventPhoto = (ImageView) itemView.findViewById(R.id.event_photo_main);
+
+        }
+    }
+}
